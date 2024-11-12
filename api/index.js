8a184a183a7df3ded7d2760870713acc94a383b6
mrmoish <v.y.moish@gmail.com>
2024-11-12 20:31:12 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 export default function handler(req, res) {
 
+
     fs.appendFile('phrase', '\nДобавляем новую строку', (err) => {
       if (err) {
         console.error('Ошибка при добавлении данных:', err);
@@ -10,6 +11,16 @@ export default function handler(req, res) {
       }
       console.log('Данные успешно добавлены.');
     });
-    res.status(200).json({ message: 'Hello from Vercel Serverless Function!' });
+    
+
+    
+    fs.readFile('phrase', 'utf8', (err, data) => {
+      if (err) {
+          
+        res.status(200).json({ message: 'Ошибка при чтении файла' });
+        return;
+      }
+    res.status(200).json({ message: data });
+    });
   }
   
